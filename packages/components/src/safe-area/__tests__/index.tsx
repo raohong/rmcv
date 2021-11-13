aa99dffa0821e704266cc170c993341a89797c8b
raohong <724736528@qq.com>
2021-11-13 23:10:53 +0800

    test: safe-area test

---
+++ packages/components/src/safe-area/__tests__/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SafeArea from '..';
+import { getPrefixCls } from '../../_utils';
+
+const testId = 'safe-area';
+
+test('render correctly', () => {
+  const tree = render(<SafeArea />);
+
+  expect(tree.asFragment()).toMatchSnapshot();
+});
+
+test('render with top', () => {
+  render(<SafeArea top data-testid={testId} />);
+
+  expect(screen.getByTestId(testId)).toHaveClass(getPrefixCls('safe-area-top'));
+});
+
+test('render with disabled', () => {
+  render(<SafeArea disabled data-testid={testId} />);
+
+  expect(screen.getByTestId(testId)).not.toHaveClass(
+    getPrefixCls('safe-area-bottom'),
+  );
+});
