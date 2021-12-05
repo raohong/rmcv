4253987a0b477afd87e28de37e4a51f3f03f12aa
raohong <724736528@qq.com>
2021-12-05 22:20:38 +0800

    feat: touchable

---
+++ packages/components/src/touchable/Touchable.tsx
@@ -0,0 +1,67 @@
+import { useDrag } from '@use-gesture/react';
+import classNames from 'classnames';
+import React, { useRef, useState } from 'react';
+import { useConfigContext } from '../config-provider';
+import { IntrinsicElementsKeys, JSXIntrinsicElementProps } from '../types';
+import { useMergeRefs, useUnmountedRef } from '../_hooks';
+import { createOverridableComponent } from '../_utils';
+
+export type TouchableProps = {
+  component?: IntrinsicElementsKeys;
+  touchDisabled?: boolean;
+  activeClassName?: string;
+  delay?: number;
+} & JSXIntrinsicElementProps<'div'>;
+
+const Touchable = React.forwardRef<HTMLDivElement, TouchableProps>(
+  (
+    {
+      component = 'div',
+      activeClassName,
+      style,
+      className,
+      delay,
+      touchDisabled,
+      ...props
+    },
+    ref,
+  ) => {
+    const { getPrefixCls } = useConfigContext();
+    const nodeRef = useRef<Element | null>(null);
+    const mergedRef = useMergeRefs(nodeRef, ref);
+    const [active, setActive] = useState(false);
+    const unmountedRef = useUnmountedRef();
+
+    useDrag(
+      ({ active: current }) => {
+        if (!unmountedRef.current) {
+          setActive(current);
+        }
+      },
+      {
+        pointer: {
+          touch: true,
+        },
+        target: nodeRef,
+        delay,
+        enabled: !touchDisabled,
+      },
+    );
+
+    return React.createElement(component, {
+      ...props,
+      ref: mergedRef,
+      style: {
+        cursor: touchDisabled ? '' : 'cursor',
+        ...style,
+      },
+      className: classNames(
+        className,
+        active && !touchDisabled && activeClassName,
+        !touchDisabled && getPrefixCls('touchable'),
+      ),
+    });
+  },
+);
+
+export default createOverridableComponent(Touchable);
---
+++ packages/components/src/touchable/index.tsx
@@ -0,0 +1,4 @@
+import './style';
+
+export type { TouchableProps } from './Touchable';
+export { default } from './Touchable';
