5f01010fa61ff68080fa4fb0265aebeeb46c040b
raohong <724736528@qq.com>
2022-02-07 22:43:16 +0800

    feat: 增加了 expand 属性

--- packages/demo/src/components/DemoBlock/index.tsx
+++ packages/demo/src/components/DemoBlock/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './index.less';
 
-const DemoBlock: React.FC<{ title?: React.ReactNode }> = ({ title, children }) => {
+const DemoBlock: React.FC<{ title?: React.ReactNode; expand?: boolean }> = ({
+  title,
+  children,
+  expand,
+}) => {
   return (
-    <section className="demo-block">
+    <section className={`demo-block ${expand ? 'demo-block-expand' : ''}`.trim()}>
       {title && <h2 className="demo-block-title">{title}</h2>}
-      {children}
+      <div className="demo-block-content">{children}</div>
     </section>
   );
 };
