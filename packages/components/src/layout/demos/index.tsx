ee393e85100e73fb9819b23a22414af216d3eb80
raohong <724736528@qq.com>
2022-02-10 17:31:19 +0800

    docs: layoutl 文档、demo

--- packages/components/src/layout/demos/index.tsx
+++ packages/components/src/layout/demos/index.tsx
@@ -1,28 +1,109 @@
+import { DemoBlock } from '@rmc-vant/demo';
 import React from 'react';
-import { Row, Col } from 'rmc-vant';
+import { Row, Col, ColProps, RowProps } from 'rmc-vant';
 
-const renderBlock = () => {
-  const colors = ['#2486ff', 'blue', 'green', 'yellow'];
+const renderDemo = (list: ColProps[], rowProps?: RowProps & { key?: string }) => {
+  const colors = ['#39a9ed', '#66c6f2'];
 
   return (
-    <div
-      style={{
-        height: 100,
-        backgroundColor: colors[Math.floor(Math.random() * colors.length)],
-      }}
-    />
+    <Row {...rowProps} style={{ marginBottom: 16 }}>
+      {list.map((item, index) => (
+        <Col {...item} key={index}>
+          <div
+            style={{
+              textAlign: 'center',
+              padding: 4,
+              background: colors[index % 2],
+              color: '#fff',
+            }}
+          >
+            {[item.offset && `offset: ${item.offset}`, `span: ${item.span}`]
+              .filter(Boolean)
+              .join(' ')}
+          </div>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
-export default () => (
-  <div>
-    <Row gutter={16}>
-      <Col span={6}>{renderBlock()}</Col>
-      <Col span={6}>{renderBlock()}</Col>
-      <Col span={6}>{renderBlock()}</Col>
-      <Col span={6}>{renderBlock()}</Col>
-      <Col span={6}>{renderBlock()}</Col>
-      <Col span={6}>{renderBlock()}</Col>
-    </Row>
-  </div>
-);
+export default () => {
+  const aligns: Exclude<RowProps['justify'], undefined>[] = [
+    'center',
+    'end',
+    'space-around',
+    'space-between',
+  ];
+
+  return (
+    <>
+      <DemoBlock title="基础用法">
+        {renderDemo([
+          {
+            span: 8,
+          },
+          {
+            span: 8,
+          },
+          {
+            span: 8,
+          },
+        ])}
+        {renderDemo([
+          {
+            span: 4,
+          },
+          {
+            span: 10,
+            offset: 4,
+          },
+        ])}
+        {renderDemo([
+          {
+            span: 12,
+            offset: 12,
+          },
+        ])}
+      </DemoBlock>
+      <DemoBlock title="设置列元素间距">
+        {renderDemo(
+          [
+            {
+              span: 8,
+            },
+            {
+              span: 8,
+            },
+            {
+              span: 8,
+            },
+          ],
+          {
+            gutter: '20px',
+          },
+        )}
+      </DemoBlock>
+      <DemoBlock title="对齐方式">
+        {aligns.map((item) =>
+          renderDemo(
+            [
+              {
+                span: 6,
+              },
+              {
+                span: 6,
+              },
+              {
+                span: 6,
+              },
+            ],
+            {
+              justify: item,
+              key: item,
+            },
+          ),
+        )}
+      </DemoBlock>
+    </>
+  );
+};
--- packages/components/src/layout/index.md
+++ packages/components/src/layout/index.md
@@ -6,8 +6,95 @@ type: 基础组件
 demo: true
 ---
 
+### 介绍
+
+`Layout` 提供了 `Row` 和 `Col` 两个组件来进行行列布局。
+
+### 引入
+
+```tsx
+import { Row, Col } from 'rmc-vant';
+```
+
+## 代码演示
+
+### 基础用法
+
+`Layout` 组件提供了 `24` 列栅格，通过在 `Col` 上添加 `span` 属性设置列所占的宽度百分比。此外，添加 `offset` 属性可以设置列的偏移宽度，计算方式与 `span` 相同。
+
+```tsx
+<>
+  <Row>
+    <Col span={8}>span: 8</Col>
+    <Col span={8}>span: 8</Col>
+    <Col span={8}>span: 8</Col>
+  </Row>
+  <Row>
+    <Col span={4}>span: 4</Col>
+    <Col span={10} offset={4}>
+      offset:4 span: 10
+    </Col>
+  </Row>
+  <Row>
+    <Col span={12} offset={12}>
+      offset:12 span: 12
+    </Col>
+  </Row>
+</>
+```
+
+### 设置列元素间距
+
+通过 gutter 属性可以设置列元素之间的间距，默认间距为 0。
+
+```tsx
+<Row gutter={20}>
+  <Col span={8}>span: 8</Col>
+  <Col span={8}>span: 8</Col>
+  <Col span={8}>span: 8</Col>
+</Row>
+```
+
+### 对齐方式
+
+通过 `justify` 属性可以设置主轴上内容的对齐方式，等价于 `flex` 布局中的 `justify-content` 属性。
+
+```tsx
+// 居中
+<Row justify="center">
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+</Row>
+
+// 右对齐
+<Row justify="end">
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+</Row>
+
+// 两端对齐
+<Row justify="space-between">
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+</Row>
+
+// 每个元素的两侧间隔相等
+<Row justify="space-around">
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+  <Col span={6}>span: 6</Col>
+</Row>
+```
+
 ## API
 
-### Props
+### Row Props
 
-{{"api": true}}
+{{"api": "Row"}}
+
+### Col Props
+
+{{"api": "Col"}}
