0b8876cd6dc0cf397d64234eff4f689ea8e33633
raohong <724736528@qq.com>
2022-02-17 19:23:10 +0800

    feat: 更多布局支持

--- packages/demo/src/components/DemoBlock/index.tsx
+++ packages/demo/src/components/DemoBlock/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './index.less';
 
-const DemoBlock: React.FC<{ title?: React.ReactNode; expand?: boolean }> = ({
-  title,
-  children,
-  expand,
-}) => {
+const DemoBlock: React.FC<{
+  title?: React.ReactNode;
+  expand?: boolean;
+  card?: boolean;
+}> = ({ title, children, expand, card }) => {
   return (
-    <section className={`demo-block ${expand ? 'demo-block-expand' : ''}`.trim()}>
+    <section
+      className={`demo-block ${expand ? 'demo-block-expand' : ''} ${
+        card ? 'demo-block-card' : ''
+      }`.trim()}
+    >
       {title && <h2 className="demo-block-title">{title}</h2>}
       <div className="demo-block-content">{children}</div>
     </section>
