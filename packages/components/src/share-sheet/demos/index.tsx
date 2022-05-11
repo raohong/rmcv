fa9268f10e5f9244c3a14eac60f64a85867318ff
raohong <724736528@qq.com>
2022-05-11 21:32:30 +0800

    docs: share-sheet docs

--- packages/components/src/share-sheet/demos/index.tsx
+++ packages/components/src/share-sheet/demos/index.tsx
@@ -1,46 +1,91 @@
+import { DemoBlock } from '@rmc-vant/demo';
 import React, { useState } from 'react';
-import { ShareSheet, ShareSheetOptions, ShareSheetIconName } from 'rmc-vant';
+import { ShareSheet, ShareSheetProps, Cell } from 'rmc-vant';
 
-export default () => {
-  const [visible, setVisible] = useState(true);
-
-  const options: ShareSheetOptions = [
-    [
-      { name: '分享链接', icon: 'limk' },
-      { name: '海报', icon: 'poster' },
-      { name: '二维码', icon: 'poster' },
-    ],
-    [
-      { name: 'QQ', icon: 'qq' },
-      { name: '小程序', icon: 'weapp-qrcode' },
-      { name: '微信', icon: 'wechat' },
-      { name: '朋友圈', icon: 'wechat-moments' },
-      { name: '微博', icon: 'weibo' },
-      { name: '分享链接', icon: 'link' },
-      { name: '海报', icon: 'poster' },
-      { name: '二维码', icon: 'qrcode' },
-      { name: 'QQ', icon: 'qq' },
-      { name: '小程序', icon: 'weapp-qrcode' },
-      { name: '小程序', icon: 'weapp-qrcode' },
-      { name: '微信', icon: 'wechat' },
-      { name: '朋友圈', icon: 'wechat-moments' },
-      { name: '微博', icon: 'weibo' },
-      { name: '分享链接', icon: 'link' },
-      { name: '海报', icon: 'poster' },
-      { name: '二维码', icon: 'qrcode' },
-      { name: 'QQ', icon: 'qq' },
-      { name: '小程序', icon: 'weapp-qrcode' },
-    ],
-  ];
+const Demo: React.FC<{ cellTitle: string } & ShareSheetProps> = ({
+  cellTitle,
+  ...props
+}) => {
+  const [visible, set] = useState(false);
 
   return (
-    <ShareSheet
-      title="立即分享给好友"
-      options={options}
-      visible={visible}
-      onClose={() => {
-        setVisible(false);
-      }}
-    />
+    <>
+      <Cell title={cellTitle} isLink onClick={() => set(true)} />
+      <ShareSheet visible={visible} onClose={() => set(false)} {...props} />
+    </>
+  );
+};
+
+export default () => {
+  return (
+    <>
+      <DemoBlock title="基础用法">
+        <Demo
+          cellTitle="显示分享面板"
+          title="立即分享给好友"
+          options={[
+            { name: '微信', icon: 'wechat' },
+            { name: '朋友圈', icon: 'wechat-moments' },
+            { name: '复制链接', icon: 'link' },
+            { name: '分享海报', icon: 'poster' },
+            { name: '二维码', icon: 'qrcode' },
+            { name: '小程序码', icon: 'weapp-qrcode' },
+          ]}
+        />
+      </DemoBlock>
+      <DemoBlock title="展示多选项">
+        <Demo
+          cellTitle="显示分享面板"
+          title="立即分享给好友"
+          options={[
+            [
+              { name: '微信', icon: 'wechat' },
+              { name: '朋友圈', icon: 'wechat-moments' },
+              { name: '微博', icon: 'weibo' },
+              { name: 'QQ', icon: 'qq' },
+            ],
+            [
+              { name: '复制链接', icon: 'link' },
+              { name: '分享海报', icon: 'poster' },
+              { name: '二维码', icon: 'qrcode' },
+              { name: '小程序码', icon: 'weapp-qrcode' },
+            ],
+          ]}
+        />
+      </DemoBlock>
+      <DemoBlock title="自定义图标">
+        <Demo
+          cellTitle="显示分享面板"
+          options={[
+            {
+              name: '名称',
+              icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-fire.png',
+            },
+            {
+              name: '名称',
+              icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-light.png',
+            },
+            {
+              name: '名称',
+              icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-water.png',
+            },
+          ]}
+        />
+      </DemoBlock>
+      <DemoBlock title="展示描述信息">
+        <Demo
+          cellTitle="显示分享面板"
+          title="立即分享给好友"
+          options={[
+            { name: '微信', icon: 'wechat' },
+            { name: '朋友圈', icon: 'wechat-moments' },
+            { name: '复制链接', icon: 'link', description: '描述信息' },
+            { name: '分享海报', icon: 'poster' },
+            { name: '二维码', icon: 'qrcode' },
+            { name: '小程序码', icon: 'weapp-qrcode' },
+          ]}
+        />
+      </DemoBlock>
+    </>
   );
 };
--- packages/components/src/share-sheet/index.md
+++ packages/components/src/share-sheet/index.md
@@ -6,6 +6,138 @@ type: 反馈组件
 demo: true
 ---
 
+### 介绍
+
+底部弹起的分享面板，用于展示各分享渠道对应的操作按钮，不含具体的分享逻辑。
+
+### 引入
+
+```tsx
+import { ShareSheet } from 'rmc-vant';
+```
+
+## 代码演示
+
+### 基础用法
+
+分享面板通过 `options` 属性来定义分享选项，数组的每一项是一个对象，对象格式见文档下方表格。
+
+```tsx
+const [visible, set] = useState(false);
+
+return (
+  <>
+    <Cell title="显示分享列表" isLink onClick={() => set(true)} />
+    <ShareSheet
+      title="分享给好友"
+      visible={visible}
+      onClose={() => set(false)}
+      options={[
+        { name: '微信', icon: 'wechat' },
+        { name: '朋友圈', icon: 'wechat-moments' },
+        { name: '复制链接', icon: 'link' },
+        { name: '分享海报', icon: 'poster' },
+        { name: '二维码', icon: 'qrcode' },
+        { name: '小程序码', icon: 'weapp-qrcode' },
+      ]}
+    />
+  </>
+);
+```
+
+### 展示多行选项
+
+当分享选项的数量较多时，可以将 `options` 定义为数组嵌套的格式，每个子数组会作为一行选项展示。
+
+```tsx
+const [visible, set] = useState(false);
+
+return (
+  <>
+    <Cell title="显示分享列表" isLink onClick={() => set(true)} />
+    <ShareSheet
+      title="分享给好友"
+      visible={visible}
+      onClose={() => set(false)}
+      options={[
+        [
+          { name: '微信', icon: 'wechat' },
+          { name: '朋友圈', icon: 'wechat-moments' },
+          { name: '微博', icon: 'weibo' },
+          { name: 'QQ', icon: 'qq' },
+        ],
+        [
+          { name: '复制链接', icon: 'link' },
+          { name: '分享海报', icon: 'poster' },
+          { name: '二维码', icon: 'qrcode' },
+          { name: '小程序码', icon: 'weapp-qrcode' },
+        ],
+      ]}
+    />
+  </>
+);
+```
+
+### 自定义图标
+
+除了使用内置的几种图标外，可以直接在 `icon` 中传入图片 URL 来使用自定义的图标
+
+```tsx
+const [visible, set] = useState(false);
+
+return (
+  <>
+    <Cell title="显示分享列表" isLink onClick={() => set(true)} />
+    <ShareSheet
+      title="分享给好友"
+      visible={visible}
+      onClose={() => set(false)}
+      options={[
+        {
+          name: '名称',
+          icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-fire.png',
+        },
+        {
+          name: '名称',
+          icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-light.png',
+        },
+        {
+          name: '名称',
+          icon: 'https://cdn.jsdelivr.net/npm/@vant/assets/custom-icon-water.png',
+        },
+      ]}
+    />
+  </>
+);
+```
+
+### 展示描述信息
+
+通过 `description` 属性可以设置标题下方的描述文字, 在 `options` 内设置 `description` 属性可以添加分享选项描述。
+
+```tsx
+const [visible, set] = useState(false);
+
+return (
+  <>
+    <Cell title="显示分享列表" isLink onClick={() => set(true)} />
+    <ShareSheet
+      title="分享给好友"
+      visible={visible}
+      onClose={() => set(false)}
+      options={[
+        { name: '微信', icon: 'wechat' },
+        { name: '朋友圈', icon: 'wechat-moments' },
+        { name: '复制链接', icon: 'link', description: '描述信息' },
+        { name: '分享海报', icon: 'poster' },
+        { name: '二维码', icon: 'qrcode' },
+        { name: '小程序码', icon: 'weapp-qrcode' },
+      ]}
+    />
+  </>
+);
+```
+
 ## API
 
 ### Props
